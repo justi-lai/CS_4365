111faa0cbf17a4fe30af2767d7281a97a2351627
Justin Lai <justin88.lai@gmail.com>
2025-04-04 12:07:10 -0500

    Fixed a spelling mistake

--- Homework 3/Knowledge_Representation/main.py
+++ Homework 3/Knowledge_Representation/main.py
@@ -17,12 +17,26 @@ def main():
             string = str(i+1) + ". " + " ".join([str(literal) for literal in clause]) + ' {' + ", ".join([str(p+1) for p in parents]) + '}'
             print(string)
         else:
-            string = str(i+1) + ". Contractiction {" + ", ".join([str(p+1) for p in parents]) + '}'
+            string = str(i+1) + ". Contradiction {" + ", ".join([str(p+1) for p in parents]) + '}'
             print(string)
     if success:
         print("Valid")
     else:
         print("Fail")
+
+    # with open("output6.txt", "w") as output_file:
+    #     for i in range(len(final_kb)):
+    #         clause, parents = final_kb[i]
+    #         if clause:
+    #             string = str(i+1) + ". " + " ".join([str(literal) for literal in clause]) + ' {' + ", ".join([str(p+1) for p in parents]) + '}'
+    #             output_file.write(string + "\n")
+    #         else:
+    #             string = str(i+1) + ". Contradiction {" + ", ".join([str(p+1) for p in parents]) + '}'
+    #             output_file.write(string + "\n")
+    #     if success:
+    #         output_file.write("Valid\n")
+    #     else:
+    #         output_file.write("Fail\n")
 
 if __name__ == "__main__":
     main()
