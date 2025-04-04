070a6cc0e2b35f266ced92ab43a5649bd2e0a927
Justin Lai <justin88.lai@gmail.com>
2025-04-04 12:59:21 -0500

    Removed testing prints

--- Homework 3/Knowledge_Representation/main.py
+++ Homework 3/Knowledge_Representation/main.py
@@ -24,19 +24,5 @@ def main():
     else:
         print("Fail")
 
-    # with open("output6.txt", "w") as output_file:
-    #     for i in range(len(final_kb)):
-    #         clause, parents = final_kb[i]
-    #         if clause:
-    #             string = str(i+1) + ". " + " ".join([str(literal) for literal in clause]) + ' {' + ", ".join([str(p+1) for p in parents]) + '}'
-    #             output_file.write(string + "\n")
-    #         else:
-    #             string = str(i+1) + ". Contradiction {" + ", ".join([str(p+1) for p in parents]) + '}'
-    #             output_file.write(string + "\n")
-    #     if success:
-    #         output_file.write("Valid\n")
-    #     else:
-    #         output_file.write("Fail\n")
-
 if __name__ == "__main__":
     main()
